eb05ff559ea508254e9ae6a7c988fb586688b7ef
Jimmy <>
2021-03-08 22:49:53 -0500

    latest

--- app.py
+++ app.py
@@ -1,1 +1,91 @@
 # SQL Alchemy Challenge!
+from flask import Flask, jsonify
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+engine = create_engine("sqlite:///Instructions/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    return (
+        f"<b/><center/>Welcome to the Climate API!</b/><br/><br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"For these, instead of start_date/end_date, type in date as YYYY-MM-DD:<br/><br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    session.close()
+    return jsonify(results)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.name).all()
+    session.close()
+    return jsonify(results)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= '2016-08-23').\
+        filter(Measurement.station == 'USC00519281').all()
+    session.close()
+
+    return jsonify(results)
+
+@app.route("/api/v1.0/<start>")
+def trip(start):
+    session = Session(engine)
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+@app.route("/api/v1.0/<start>/<end>")
+def trip_2(start,end):
+    session = Session(engine)
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date= dt.datetime.strptime(end,'%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end = end_date-last_year
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
